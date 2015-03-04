e1d09a2d13e00999dd32cf94eecb2370124a24e4
mogh <mogh@example.com>
2015-03-04 12:30:44 -0500

    added author_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ BookManager3::Application.routes.draw do
   # Generated model routes
   resources :books
   # need routes for authors and categories ...
+  resources :authors
   
   # Additional routes we've created for this project
   get 'proposed' => 'books#proposed', as: :proposed_books
